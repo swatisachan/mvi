87f02b384f1f38eafa1128a41f58a8e8c8aa91d0
swati <swatiinfosys86@gmail.com>
2019-05-05 01:06:10 -0700

    t1

---
+++ mvi/src/test/java/grp/mvi/NewTest3.java
@@ -0,0 +1,13 @@
+package grp.mvi;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class NewTest3 {
+  @Test
+  public void f() {
+	  
+	  assertEquals("swati", "swati");
+  }
+}
